ded94d1787e4b0979364c98369f900fe197be18f
Al Kent <al_kent@mastercard.com>
2017-08-29 15:17:25 -0500

    testing

--- gateway.php
+++ gateway.php
@@ -40,15 +40,15 @@ else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         )
     );
 
-    $options['http']['method'] = 'PUT';
-    $options['http']['content'] = json_encode($data);
-
-    $orderId = uniqid("", true);
-    $txnId = uniqid("", true);
-    $url = $gatewayUrl . '/order/' . $orderId . '/transaction/' . $txnId;
-    $result = file_get_contents($url, false, $context);
-
-    var_dump($result);
+    // $options['http']['method'] = 'PUT';
+    // $options['http']['content'] = json_encode($data);
+    //
+    // $orderId = uniqid("", true);
+    // $txnId = uniqid("", true);
+    // $url = $gatewayUrl . '/order/' . $orderId . '/transaction/' . $txnId;
+    // $result = file_get_contents($url, false, $context);
+    //
+    // var_dump($result);
     exit;
 }
 
