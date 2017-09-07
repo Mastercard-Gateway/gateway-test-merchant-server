c0e05cc9a7cced595b71dd235b08577f16b2dfed
Al Kent <al.kent@mastercard.com>
2017-09-07 16:14:00 -0500

    updating pageUrl script outputting url on index page

--- _bootstrap.php
+++ _bootstrap.php
@@ -64,8 +64,23 @@ $headers = array(
 );
 
 // construct page url
-$pageUrl = "http".(!empty($_SERVER['HTTPS'])?"s":"")."://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+$pageUrl = full_url($_SERVER);
 $docsUrl = "https://$regionPrefix-gateway.mastercard.com/api/documentation/apiDocumentation/rest-json/version/latest/api.html";
+
+function url_origin( $s, $use_forwarded_host = false ) {
+    $ssl      = ( ! empty( $s['HTTPS'] ) && $s['HTTPS'] == 'on' );
+    $sp       = strtolower( $s['SERVER_PROTOCOL'] );
+    $protocol = substr( $sp, 0, strpos( $sp, '/' ) ) . ( ( $ssl ) ? 's' : '' );
+    $port     = $s['SERVER_PORT'];
+    $port     = ( ( ! $ssl && $port=='80' ) || ( $ssl && $port=='443' ) ) ? '' : ':'.$port;
+    $host     = ( $use_forwarded_host && isset( $s['HTTP_X_FORWARDED_HOST'] ) ) ? $s['HTTP_X_FORWARDED_HOST'] : ( isset( $s['HTTP_HOST'] ) ? $s['HTTP_HOST'] : null );
+    $host     = isset( $host ) ? $host : $s['SERVER_NAME'] . $port;
+    return $protocol . '://' . $host;
+}
+
+function full_url( $s, $use_forwarded_host = false ) {
+    return url_origin( $s, $use_forwarded_host ) . $s['REQUEST_URI'];
+}
 
 function intercept($method) {
     return strcasecmp($_SERVER['REQUEST_METHOD'], $method) == 0;
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+include '_bootstrap.php';
+?>
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
@@ -9,7 +12,8 @@
     </head>
     <body>
         <h1>Mastercard Gateway Sample Merchant Server</h1>
-        <p>This is an sample application to help developers start building mobile applications using the Mastercard Gateway mobile SDK.</p>
+        <p>This is an sample application to help developers start building mobile applications using the Mastercard Gateway mobile SDK.<br/>If you are using the sample app, include the following url in your configuration:</p>
+        <pre><?php echo htmlentities($pageUrl); ?></pre>
         <h3>Available APIs</h3>
         <ul>
             <li><a href="./session.php">Session API</a></li>
