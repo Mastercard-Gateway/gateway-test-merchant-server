60a11252d7cad33984ab918d3fbe42c8180b6473
Al Kent <al.kent@mastercard.com>
2017-09-19 10:54:14 -0500

    include bootstrap on index page to indicate config errors.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include '_bootstrap.php'; ?>
 <html>
     <head>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
