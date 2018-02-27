eee9fe0be8fef0b147c985514764ce3eeec54c01
Josh Monroe <josh_monroe@mastercard.com>
2018-02-27 09:14:01 -0600

    Initial support for the 3ds check

---
+++ 3dsCheck.php
@@ -0,0 +1,80 @@
+<?php
+
+/*
+ * Copyright (c) 2016 Mastercard
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include '_bootstrap.php';
+
+// proxy PUT requests
+if (intercept('PUT')) {
+    // build path
+    $threeDSId = requiredQueryParam('3DSecureId');
+    $path = '/3DSecureId/' . $threeDSId;
+
+    proxyCall($path);
+}
+
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+        <style>
+            body {
+                padding: 2rem;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>3DS API</h1>
+        <h3>Check 3DS Enrollment</h3>
+        <h5>Sample Request</h5>
+        <pre><code>PUT <?php echo htmlentities($pageUrl . '?3DSecureId={3DSId}'); ?>
+
+Content-Type: application/json
+Payload:
+{
+    "order": {
+    	"amount": "1.00",
+    	"currency": "USD"
+    },
+    "session": {
+    	"id": "SESSION0000000000000000000000"
+    },
+    "3DSecure": {
+    	"authenticationRedirect": {
+        "responseUrl" : "myApp://someRoute"
+      }
+    }
+}</code></pre>
+
+        <h5>Response</h5>
+        <pre><code>Content-Type: application/json
+Payload:
+{
+    "apiVersion": "<?php echo $apiVersion; ?>",
+    "gatewayResponse": {
+        "3DSecure": {
+          "summaryStatus": "CARD_ENROLLED"
+        },
+        "3DSecureId": "<?php echo $threeDSId; ?>",
+        "merchant": "<?php echo $merchantId; ?>",
+        "response": { ... },
+        "version": "<?php echo $apiVersion; ?>"
+    }
+}</code></pre>
+    </body>
+</html>
