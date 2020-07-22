ba41c330e4efd4ad0d78ac2239daf72ca73a5e60
caigangying <42607450+caigangying@users.noreply.github.com>
2020-07-22 17:49:36 +0800

    Update _bootstrap.php
    取消 商户编号test 的限制

--- _bootstrap.php
+++ _bootstrap.php
@@ -26,9 +26,7 @@ $apiVersion = getenv('GATEWAY_API_VERSION');
 
 // merchant id must be TEST
 $merchantIdPrefix = substr($merchantId, 0, 4);
-if (strcasecmp($merchantIdPrefix, "test") != 0) {
-    error(500, 'Only TEST merchant IDs should be used with this software');
-}
+
 
 // get regional url prefix
 $prefix = 'mtf.';
