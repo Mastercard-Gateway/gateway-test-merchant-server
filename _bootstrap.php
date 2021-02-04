21788e1c04a63053e6df8c6df3fe9869665d0988
Al Kent <al.kent@mastercard.com>
2021-02-04 16:22:21 -0600

    changing environment prefixes

--- _bootstrap.php
+++ _bootstrap.php
@@ -31,29 +31,29 @@ $merchantIdPrefix = substr($merchantId, 0, 4);
 // }
 
 // get regional url prefix
-$prefix = 'test-';
+$prefix = 'mtf';
 if (strcasecmp($region, "ASIA_PACIFIC") == 0) {
-    $prefix = 'ap-';
+    $prefix = 'ap';
 } else if (strcasecmp($region, "EUROPE") == 0) {
-    $prefix = 'eu-';
+    $prefix = 'eu';
 } else if (strcasecmp($region, "NORTH_AMERICA") == 0) {
-    $prefix = 'na-';
+    $prefix = 'na';
 } else if (strcasecmp($region, "MTF") == 0) {
-    $prefix = 'test-';
+    $prefix = 'mtf';
 } else if (strcasecmp($region, "QA01") == 0) {
-    $prefix = 'qa01.';
+    $prefix = 'qa01';
 } else if (strcasecmp($region, "QA02") == 0) {
-    $prefix = 'qa02.';
+    $prefix = 'qa02';
 } else if (strcasecmp($region, "QA03") == 0) {
-    $prefix = 'qa03.';
+    $prefix = 'qa03';
 } else if (strcasecmp($region, "QA04") == 0) {
-    $prefix = 'qa04.';
+    $prefix = 'qa04';
 } else if (strcasecmp($region, "QA05") == 0) {
-    $prefix = 'qa05.';
+    $prefix = 'qa05';
 } else if (strcasecmp($region, "QA06") == 0) {
-    $prefix = 'qa06.';
+    $prefix = 'qa06';
 } else if (strcasecmp($region, "PEAT") == 0) {
-    $prefix = 'perf.';
+    $prefix = 'perf';
 } else {
     error(500, "Invalid region provided. Valid values include ASIA_PACIFIC, EUROPE, NORTH_AMERICA, MTF");
 }
@@ -64,7 +64,7 @@ if (intval($apiVersion) < 39) {
 }
 
 // build api endpoint url
-$gatewayUrl = "https://${prefix}gateway.mastercard.com/api/rest/version/${apiVersion}/merchant/${merchantId}";
+$gatewayUrl = "https://${prefix}.gateway.mastercard.com/api/rest/version/${apiVersion}/merchant/${merchantId}";
 
 // parse query string
 $query = array();
