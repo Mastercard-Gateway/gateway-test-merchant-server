08f556c19dde73c565fc10ca062179c816daadbc
Firoz Khan Pathan [C] <firozkhan.pathan@mastercard.com>
2025-07-25 20:23:09 +0530

    redirect URL for Reterieve Order try 2

--- retrieve-order.php
+++ retrieve-order.php
@@ -28,35 +28,6 @@ if (intercept('GET')) {
     error_log($response);
 
     // build mobile redirect with full response payload as acsResult
-//    doRedirect("gatewaysdk://3dsecure?acsResult=" . urlencode($response));
-    <?php
-    $customUrl = "gatewaysdk://3dsecure?acsResult=" . urlencode($response);
-    ?>
-
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <title>Redirecting...</title>
-        <script>
-            // Try to navigate to custom scheme after page loads
-            window.onload = function() {
-                // Redirect to your custom scheme
-                window.location = "<?php echo $customUrl; ?>";
-
-                // Fallback: after 2 seconds, show a message or fallback URL
-                setTimeout(function() {
-                    document.getElementById('message').style.display = 'block';
-                }, 2000);
-            };
-        </script>
-    </head>
-    <body>
-        <p>Redirecting you to the payment gateway...</p>
-        <p id="message" style="display:none;">
-            If you are not redirected automatically, <a href="<?php echo $customUrl; ?>">click here</a>.
-        </p>
-    </body>
-    </html>
+    doRedirect("gatewaysdk://3dsecure?acsResult=" . urlencode($response));
 }
 
